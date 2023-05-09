82b5ae6610fb9e1217460e2fac7e2c31ac0d2ced
Beck Busch <beckbusch5@gmail.com>
2023-05-09 23:44:16 +1200

    Reworked page with index and frame

---
+++ src/Components/main.js
@@ -0,0 +1,7 @@
+navString = `<a href="/index.html"><div class="navItem">Home</div></a>
+<a href="/Pages/repos.html"><div class="navItem">Repos</div></a>
+<a href="/Pages/embedded.html"><div class="navItem">Embedded</div></a>
+<a href="/Pages/vhdl.html"><div class="navItem">VHDL</div></a>
+<a href="/Pages/psoc.html"><div class="navItem">PSOC</div></a>`
+
+document.getElementById("navigation").innerHTML = navString;
